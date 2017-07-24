da4bb183856c680b20f85f2c65f220fe80452137
saimunshahee <saimunshahee@Saimuns-Mac.local>
2017-07-24 17:05:47 -0400

    Adding ReadDB.py class skeleton

---
+++ ReadDB/ReadDB.py
@@ -0,0 +1,20 @@
+import Client from Client
+
+def ReadDB():
+	client = Client()
+	otherargs = ""
+	isType2, paired, isleft, noclose = False, False, False, False
+
+	def __init__(self):
+	
+	def parseArgs(self,args):
+		return NotImplemented
+	
+	def printHelp():
+		return NotImplemented
+	
+	def run():
+		return NotImplemented
+
+if __name__ == "__main__":
+	print "Not done yet"
